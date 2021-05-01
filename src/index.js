e6ab66366a04b1de06e854ae29b190c47fe14a3a
takesyhi <51203374+lenna-kun@users.noreply.github.com>
2021-05-01 23:23:31 +0900

    Revert "React環境構築、トップページ作成"

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { init } from './scripts/init';
-import Frame from './components/Frame';
-import './styles/index.scss';
-
-init();
-const appRoot = document.getElementById('shogi__root');
-ReactDOM.render(<Frame />, appRoot);
--- src/scripts/init.js
+++
@@ -1,7 +0,0 @@
-export const init = () => {
-    // CSSで100vhの代わりにカスタブオブジェクトvhを利用(スマホ対策)
-    document.documentElement.style.setProperty('--vh', window.innerHeight + 'px');
-    window.onresize = () => {
-        document.documentElement.style.setProperty('--vh', window.innerHeight + 'px');
-    };
-  };
